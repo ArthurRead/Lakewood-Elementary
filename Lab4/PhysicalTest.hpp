302fe5dfe8390209eae2dd012f56b90de54f2fea
Borys <Borys@borys-6f3fed00f.gateway.2wire.net>
2013-11-21 17:33:09 -0500

    Mapping out classes for Physical Tests

--- Lab4/PhysicalTest.hpp
+++ Lab4/PhysicalTest.hpp
@@ -18,44 +18,79 @@ class PhysicalTests {
 	int checkweight;
 	int checkheight;
 
+	int redbloodcells;
+	int whitebloodcells;
 
-//this will contain variables related to the three derived classes
-	//i.e. such as a needle or some instrument used for three of the tests.
 
 };
 
-class VitalSignsTest : public PhysicalTests{
+class VitalSignsTest : public PhysicalTests{	//for sure this one as well
 
 	int theromemeter;
 	int bloodpressuremonitor;
 	int stethescope;	//for breathing
+	string methodoftakingtemperature;
+	int pulse;
 
 
-void BloodPressure();
-void Pulse();
-void BodyTemp();
-void Breathing();
+
+	void BloodPressure();
+	void HeartRhythym();	//pulse uses bloodpressure to determine strength, and pulse.
+	void BodyTemp();
+	void Breathing();
 
 };
 
 class UrineSampleTest : public PhysicalTests{
+	int phValue;	//could be in base class, however needs to be used
 
+	int Proteins;	//used
+
+	int sugar; 		//used
+	int ketone;		//used
+
+	int nitrate;	//used
+
+
+	int bilirubin; //(breaks down hemoglobin. Defective = bad blood.	NOT USED YEt!!!!
+
+
+void Inflamationcheck(/*protein level*/);	//high protein = inflamation in kidney
+void diabetuscheck(/*int sugar and ketone*/);		//high ketone and sugar = diabetus
+void bacterialcheck(/*int whitebloodcells and nitrite*/);		//high white bloodcells = bacterial infection (USE WHITEBLOODCELLS FROM BASE)
+};
+
+class NonVitalsTest : public PhysicalTests {
+
+	void Skinapperance(); //maybe?
+	void earcheck_hearingcheck();
+	void paincheck();
+	void eyesightcheck();
+
+	void nosecheck(); //related to breathing eventually, maybe remove?
 
 
 
 };
 
-class EyesightTest : public PhysicalTests {
+class MobilityTest : public PhysicalTests {	//definetly this one
 
 
+	void neckmovement();
+	void armmovement();
+	void legmovement();
+	void spinmovement();
+};
+
+class NervousSystem : public PhysicalTests {	//maybe don't do this class?
+
+	void checkmentalstatus();
+	void checkreflexes(); 	//related to mobility and responsivness
+	void checkcranialnerves ();
+	void checksensorysystem();
 
 
 };
-
-
-
-
-
 
 
 
