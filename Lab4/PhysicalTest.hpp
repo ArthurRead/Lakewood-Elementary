cd4fa4bf519cd557c8382c0f8c3f8c81a1c1f1d0
Borys <Borys@borys-6f3fed00f.gateway.2wire.net>
2013-11-20 23:44:24 -0500

    Started Physical Tests

---
+++ Lab4/PhysicalTest.hpp
@@ -0,0 +1,46 @@
+/*
+ * PhysicalTest.hpp
+ *
+ *  Created on: Nov 20, 2013
+ *      Author: Borys
+ */
+
+#ifndef PHYSICALTEST_HPP_
+#define PHYSICALTEST_HPP_
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class PhysicalTests {
+
+//this will contain variables related to the three derived classes
+	//i.e. such as a needle or some instrument used for three of the tests.
+
+};
+
+class VitalSignsTest : public PhysicalTests{
+
+
+
+};
+
+class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* PHYSICALTEST_HPP_ */
