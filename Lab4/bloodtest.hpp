0419a32f93abe39096f7bb08e2ea0d12f3f3e06d
Alan Quach <alanquach2000@hotmail.com>
2013-11-25 20:31:32 -0500

    added functions to CompleteBloodCount class

--- Lab4/bloodtest.hpp
+++ Lab4/bloodtest.hpp
@@ -17,8 +17,8 @@ using namespace std;
 class BloodTest{
 protected:
 	string Bloodtype; // A, B, O, AB
-	string Gender; // male, female
-	int Age;
+	string Gender; // male, female, N/A
+	//int Age;
 public:
 	BloodTest(string BT, string G){Bloodtype = BT; Gender = G;}
 };
@@ -34,10 +34,16 @@ private:
 	int Hemoglobin; // units: (gm/dL) (grams/deciliter)
 	int Hematocrit; // units: (%)
 	int MCV; // Mean Corpuscular Volume, units: femtoliter
+	int rRBC, rWBC, rPlatelets, rHemoglobin, rHematocrit, rMCV; // results for ranges of the variable 1-normal, 0-notnormal
 public:
 	CompleteBloodCount(string BT, string G, int R, int W, int P, int Hemo, int Hema, int M);
-	void show(){cout << RBC << " " << WBC << " " << Platelets << " " << Hemoglobin << " " << Hematocrit << " " << MCV << " " << Gender << " "<< Bloodtype;}
-	//function: normal ranges for males and females
+	CompleteBloodCount(string G, int R, int W, int P, int Hemo, int Hema, int M);
+		// Bloodtype not needed for these tests
+	void BloodCellRange();
+	void PlateletsRange();
+	void HemRange();
+	void MCVRange();
+	void TESTALL();
 };
 
 class BloodChemistryTest : public BloodTest{
@@ -47,6 +53,7 @@ private:
 	int Calcium;
 	int Electrolytes;
 public:
+	BloodChemistryTest(int G, int C, int E);
 	//function: diabetes diagnosis
 };
 
@@ -61,7 +68,6 @@ private:
 public:
 	HeartTest(string BT, string G, int T, int L, int H);
 	HeartTest(int T, int L, int H); // Gender and bloodtype not necessary for this test
-	void show(){cout<<Total<<" "<<LDL<<" "<<HDL<<" "<<Bloodtype << " "<< Gender;}
 };
 
 
@@ -78,10 +84,100 @@ CompleteBloodCount::CompleteBloodCount(string BT, string G, int R, int W, int P,
 	MCV = M;
 }
 
+CompleteBloodCount::CompleteBloodCount(string G, int R, int W, int P, int Hemo, int Hema, int M)
+:BloodTest("N/A",G)
+{
+	RBC = R;
+	WBC = W;
+	Platelets = P;
+	Hemoglobin = Hemo;
+	Hematocrit = Hema;
+	MCV = M;
+}
+
+void CompleteBloodCount::BloodCellRange()
+{
+	if(Gender == "male")
+	{
+		if(RBC>=5000000 && RBC<=6000000)
+			rRBC = 1; // normal
+		else
+			rRBC = 0; // not normal
+	}
+
+	if(Gender == "female")
+	{
+		if(RBC>=4000000 && RBC<=5000000)
+			rRBC = 1; // normal
+		else
+			rRBC = 0; // not normal
+	}
+
+	if(WBC>=4500 && WBC<=10000) // same for males and females
+		rWBC = 1;
+	else
+		rWBC = 0;
+}
+
+void CompleteBloodCount::PlateletsRange()
+{
+	if(Platelets>=140000 && Platelets<=450000)
+		rPlatelets = 1;
+	else
+		rPlatelets = 0;
+}
+
+void CompleteBloodCount::HemRange()
+{
+	if(Gender == "male")
+	{
+		if(Hemoglobin>=14 && Hemoglobin<=17)
+			rHemoglobin = 1;
+		else
+			rHemoglobin = 0;
+		if(Hematocrit>=41 && Hematocrit<=50)
+			rHematocrit = 1;
+		else
+			rHematocrit = 0;
+	}
+	if(Gender == "female")
+	{
+		if(Hemoglobin>=12 && Hemoglobin<=15)
+			rHemoglobin = 1;
+		else
+			rHemoglobin = 0;
+		if(Hematocrit>=36 && Hematocrit<=44)
+			rHematocrit = 1;
+		else
+			rHematocrit = 0;
+	}
+}
+
+void CompleteBloodCount::MCVRange()
+{
+	if(MCV>=80 && MCV<=95)
+		rMCV = 1;
+	else
+		rMCV = 0;
+}
+
+void CompleteBloodCount::TESTALL()
+{
+	BloodCellRange();
+	PlateletsRange();
+	HemRange();
+	MCVRange();
+}
 
 
-/********** **********/
-
+/********** Functions for BloodChemistryTest class **********/
+BloodChemistryTest::BloodChemistryTest(int G, int C, int E)
+:BloodTest("N/A","N/A")
+{
+	Glucose = G;
+	Calcium = C;
+	Electrolytes = E;
+}
 
 
 /********** Functions for HeartTest class **********/
