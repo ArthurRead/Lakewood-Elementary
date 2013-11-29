a5d0fa965f677bd86a918af962a9828728eb1ab6
Borys O <Borys O@BorysO-HP.gateway.2wire.net>
2013-11-29 18:28:06 -0500

    Finished second class prototypes, Urine Sample tests

--- Lab4/PhysicalTest.hpp
+++ Lab4/PhysicalTest.hpp
@@ -19,8 +19,7 @@ protected:
 	int checkweight;	//weight in lbs
 	int checkheight;	//height in meters
 	int age;	// patients age
-	int redbloodcells;
-	int whitebloodcells;
+
 	int color;
 
 public:
@@ -37,17 +36,18 @@ private:
 	int pulse;	//heart beats/min
 	int systolic;	//pressure in Hg (top number in blood pressure reading)
 	int diastolic;	//pressure in Hg (bottom number in blood pressure reading)
+	int breathrate;	//breaths / min
+	//**************status checking********************
 	int HL_BloodPressure;	//0  normal, 1 warning for high, 2 high blood, -1 low blood - patient status for BP
 	int Heartcondition; //0 athlete, 1 excellent, 2 good, 3 above average, 4 average, 5 below, 6 poor - patient status for HC
 	int Tempregulation;	//0 normal, 1 higher, 2 high, 3 dangerous, -1 below normal - patient temperature status
-	int breathrate;	//breaths / min
 	int respiratory; //0 normal, -1 below normal, 1 higher than normal - patients breathing status
 public:
 	void BloodPressure();
 	void HeartBeat();
 	void BodyTemp();
 	void Breathing();
-	void VitalsCheck();
+	void VitalsCheck();	//uses all of the tests and makes a conclusion to the data recieved from all tests in vital signs
 
 };
 
@@ -55,11 +55,17 @@ class UrineSampleTest : public PhysicalTests{
 private:
 	int phValue;	//could be in base class, however needs to be use
 	int Proteins;	//used
-	int sugar; 		//used
-	int ketone;		//used
+	int sugar; 		//in mg/dL
+	int ketone;		//in mg/dL
 	int nitrate;	//used
 	int bilirubin; //(breaks down hemoglobin. Defective = bad blood.	NOT USED YEt!!!!
-
+	int ketonecheck;	//is ketone present or not 1 yes, 0 no
+	int diabetescheck; //0 maybe, 1 yes, -1 no
+	int redbloodcells;
+	int whitebloodcells;
+	//**************status checking***************************
+	int infected; //0 maybe, 1 yes, -1 no
+	int inflamed; //-1 not inflamed, 1 inflamed kidneys
 public:
 	void Inflamationcheck(/*protein level*/);	//high protein = inflamation in kidney
 	void diabetuscheck(/*int sugar and ketone*/);		//high ketone and sugar = diabetus
@@ -116,9 +122,15 @@ class NervousSystem : public PhysicalTests {	//maybe don't do this class?
 };
 
 
-//	void Breathing();
+//*******************************************************************************************************
+//**********************************LAB 4 ENDS HERE (Based on what's required)****************************
+//*******************************************************************************************************
+//*******************************************************************************************************
 
 
+
+
+//***********Project samples*************
 void VitalSignsTest::BloodPressure(){
 	if (bloodpressuremonitor == 1){	//if bloodpressure monitor used
 
@@ -156,124 +168,124 @@ void VitalSignsTest::HeartBeat(){
 
 		if (gender == 'male'){
 			if (age >=18 && age <=25){
-				if (pulse >= 49 && pulse >= 55){Heartcondition == 0;}
-				if (pulse >= 56 && pulse >= 61){Heartcondition == 1;}
-				if (pulse >= 62 && pulse >= 65){Heartcondition == 2;}
-				if (pulse >= 66 && pulse >= 69){Heartcondition == 3;}
-				if (pulse >= 70 && pulse >= 73){Heartcondition == 4;}
-				if (pulse >= 74 && pulse >= 81){Heartcondition == 5;}
-				if (pulse >= 82){Heartcondition == 6;}
+				if (pulse >= 49 && pulse >= 55){Heartcondition = 0;}
+				if (pulse >= 56 && pulse >= 61){Heartcondition = 1;}
+				if (pulse >= 62 && pulse >= 65){Heartcondition = 2;}
+				if (pulse >= 66 && pulse >= 69){Heartcondition = 3;}
+				if (pulse >= 70 && pulse >= 73){Heartcondition = 4;}
+				if (pulse >= 74 && pulse >= 81){Heartcondition = 5;}
+				if (pulse >= 82){Heartcondition = 6;}
 			}
 			if (age >=26 && age <=35){
-				if (pulse >= 49 && pulse >= 54){Heartcondition == 0;}
-				if (pulse >= 55 && pulse >= 61){Heartcondition == 1;}
-				if (pulse >= 62 && pulse >= 65){Heartcondition == 2;}
-				if (pulse >= 66 && pulse >= 70){Heartcondition == 3;}
-				if (pulse >= 71 && pulse >= 74){Heartcondition == 4;}
-				if (pulse >= 75 && pulse >= 81){Heartcondition == 5;}
-				if (pulse >= 82){Heartcondition == 6;}
+				if (pulse >= 49 && pulse >= 54){Heartcondition = 0;}
+				if (pulse >= 55 && pulse >= 61){Heartcondition = 1;}
+				if (pulse >= 62 && pulse >= 65){Heartcondition = 2;}
+				if (pulse >= 66 && pulse >= 70){Heartcondition = 3;}
+				if (pulse >= 71 && pulse >= 74){Heartcondition = 4;}
+				if (pulse >= 75 && pulse >= 81){Heartcondition = 5;}
+				if (pulse >= 82){Heartcondition = 6;}
 			}
 			if (age >=36 && age <=45){
-				if (pulse >= 59 && pulse >= 56){Heartcondition == 0;}
-				if (pulse >= 57 && pulse >= 62){Heartcondition == 1;}
-				if (pulse >= 63 && pulse >= 66){Heartcondition == 2;}
-				if (pulse >= 67 && pulse >= 70){Heartcondition == 3;}
-				if (pulse >= 71 && pulse >= 75){Heartcondition == 4;}
-				if (pulse >= 76 && pulse >= 82){Heartcondition == 5;}
-				if (pulse >= 83){Heartcondition == 6;}
+				if (pulse >= 59 && pulse >= 56){Heartcondition = 0;}
+				if (pulse >= 57 && pulse >= 62){Heartcondition = 1;}
+				if (pulse >= 63 && pulse >= 66){Heartcondition = 2;}
+				if (pulse >= 67 && pulse >= 70){Heartcondition = 3;}
+				if (pulse >= 71 && pulse >= 75){Heartcondition = 4;}
+				if (pulse >= 76 && pulse >= 82){Heartcondition = 5;}
+				if (pulse >= 83){Heartcondition = 6;}
 			}
 			if (age >=46 && age <=55){
-				if (pulse >= 50 && pulse >= 57){Heartcondition == 0;}
-				if (pulse >= 58 && pulse >= 63){Heartcondition == 1;}
-				if (pulse >= 64 && pulse >= 67){Heartcondition == 2;}
-				if (pulse >= 68 && pulse >= 71){Heartcondition == 3;}
-				if (pulse >= 72 && pulse >= 76){Heartcondition == 4;}
-				if (pulse >= 77 && pulse >= 83){Heartcondition == 5;}
-				if (pulse >= 84){Heartcondition == 6;}
+				if (pulse >= 50 && pulse >= 57){Heartcondition = 0;}
+				if (pulse >= 58 && pulse >= 63){Heartcondition = 1;}
+				if (pulse >= 64 && pulse >= 67){Heartcondition = 2;}
+				if (pulse >= 68 && pulse >= 71){Heartcondition = 3;}
+				if (pulse >= 72 && pulse >= 76){Heartcondition = 4;}
+				if (pulse >= 77 && pulse >= 83){Heartcondition = 5;}
+				if (pulse >= 84){Heartcondition = 6;}
 			}
 			if (age >=56 && age <=65){
-				if (pulse >= 51 && pulse >= 56){Heartcondition == 0;}
-				if (pulse >= 57 && pulse >= 61){Heartcondition == 1;}
-				if (pulse >= 62 && pulse >= 67){Heartcondition == 2;}
-				if (pulse >= 68 && pulse >= 71){Heartcondition == 3;}
-				if (pulse >= 72 && pulse >= 75){Heartcondition == 4;}
-				if (pulse >= 76 && pulse >= 81){Heartcondition == 5;}
-				if (pulse >= 82){Heartcondition == 6;}
+				if (pulse >= 51 && pulse >= 56){Heartcondition = 0;}
+				if (pulse >= 57 && pulse >= 61){Heartcondition = 1;}
+				if (pulse >= 62 && pulse >= 67){Heartcondition = 2;}
+				if (pulse >= 68 && pulse >= 71){Heartcondition = 3;}
+				if (pulse >= 72 && pulse >= 75){Heartcondition = 4;}
+				if (pulse >= 76 && pulse >= 81){Heartcondition = 5;}
+				if (pulse >= 82){Heartcondition = 6;}
 			}
 			if (age >= 66){
-				if (pulse >= 50 && pulse >= 55){Heartcondition == 0;}
-				if (pulse >= 56 && pulse >= 61){Heartcondition == 1;}
-				if (pulse >= 62 && pulse >= 65){Heartcondition == 2;}
-				if (pulse >= 66 && pulse >= 69){Heartcondition == 3;}
-				if (pulse >= 70 && pulse >= 73){Heartcondition == 4;}
-				if (pulse >= 74 && pulse >= 79){Heartcondition == 5;}
-				if (pulse >= 80){Heartcondition == 6;}
+				if (pulse >= 50 && pulse >= 55){Heartcondition = 0;}
+				if (pulse >= 56 && pulse >= 61){Heartcondition = 1;}
+				if (pulse >= 62 && pulse >= 65){Heartcondition = 2;}
+				if (pulse >= 66 && pulse >= 69){Heartcondition = 3;}
+				if (pulse >= 70 && pulse >= 73){Heartcondition = 4;}
+				if (pulse >= 74 && pulse >= 79){Heartcondition = 5;}
+				if (pulse >= 80){Heartcondition = 6;}
 			}
 			else {
 				cout << "Age too low or heart beat much too low!";
-				Heartcondition == 6;
+				Heartcondition = 6;
 			}
 		}
 		if (gender == 'female'){
 
 			if (age >=18 && age <=25){
-				if (pulse >= 54 && pulse >= 60){Heartcondition == 0;}
-				if (pulse >= 61 && pulse >= 65){Heartcondition == 1;}
-				if (pulse >= 66 && pulse >= 69){Heartcondition == 2;}
-				if (pulse >= 70 && pulse >= 73){Heartcondition == 3;}
-				if (pulse >= 74 && pulse >= 78){Heartcondition == 4;}
-				if (pulse >= 79 && pulse >= 84){Heartcondition == 5;}
-				if (pulse >= 85){Heartcondition == 6;}
+				if (pulse >= 54 && pulse >= 60){Heartcondition = 0;}
+				if (pulse >= 61 && pulse >= 65){Heartcondition = 1;}
+				if (pulse >= 66 && pulse >= 69){Heartcondition = 2;}
+				if (pulse >= 70 && pulse >= 73){Heartcondition = 3;}
+				if (pulse >= 74 && pulse >= 78){Heartcondition = 4;}
+				if (pulse >= 79 && pulse >= 84){Heartcondition = 5;}
+				if (pulse >= 85){Heartcondition = 6;}
 			}
 			if (age >=26 && age <=35){
-				if (pulse >= 54 && pulse >= 59){Heartcondition == 0;}
-				if (pulse >= 60 && pulse >= 64){Heartcondition == 1;}
-				if (pulse >= 65 && pulse >= 68){Heartcondition == 2;}
-				if (pulse >= 69 && pulse >= 72){Heartcondition == 3;}
-				if (pulse >= 73 && pulse >= 76){Heartcondition == 4;}
-				if (pulse >= 77 && pulse >= 82){Heartcondition == 5;}
-				if (pulse >= 83){Heartcondition == 6;}
+				if (pulse >= 54 && pulse >= 59){Heartcondition = 0;}
+				if (pulse >= 60 && pulse >= 64){Heartcondition = 1;}
+				if (pulse >= 65 && pulse >= 68){Heartcondition = 2;}
+				if (pulse >= 69 && pulse >= 72){Heartcondition = 3;}
+				if (pulse >= 73 && pulse >= 76){Heartcondition = 4;}
+				if (pulse >= 77 && pulse >= 82){Heartcondition = 5;}
+				if (pulse >= 83){Heartcondition = 6;}
 			}
 			if (age >=36 && age <=45){
-				if (pulse >= 54 && pulse >= 59){Heartcondition == 0;}
-				if (pulse >= 60 && pulse >= 64){Heartcondition == 1;}
-				if (pulse >= 65 && pulse >= 69){Heartcondition == 2;}
-				if (pulse >= 70 && pulse >= 73){Heartcondition == 3;}
-				if (pulse >= 74 && pulse >= 78){Heartcondition == 4;}
-				if (pulse >= 79 && pulse >= 84){Heartcondition == 5;}
-				if (pulse >= 85){Heartcondition == 6;}
+				if (pulse >= 54 && pulse >= 59){Heartcondition = 0;}
+				if (pulse >= 60 && pulse >= 64){Heartcondition = 1;}
+				if (pulse >= 65 && pulse >= 69){Heartcondition = 2;}
+				if (pulse >= 70 && pulse >= 73){Heartcondition = 3;}
+				if (pulse >= 74 && pulse >= 78){Heartcondition = 4;}
+				if (pulse >= 79 && pulse >= 84){Heartcondition = 5;}
+				if (pulse >= 85){Heartcondition = 6;}
 			}
 			if (age >=46 && age <=55){
-				if (pulse >= 54 && pulse >= 60){Heartcondition == 0;}
-				if (pulse >= 61 && pulse >= 65){Heartcondition == 1;}
-				if (pulse >= 66 && pulse >= 69){Heartcondition == 2;}
-				if (pulse >= 70 && pulse >= 73){Heartcondition == 3;}
-				if (pulse >= 74 && pulse >= 77){Heartcondition == 4;}
-				if (pulse >= 78 && pulse >= 83){Heartcondition == 5;}
-				if (pulse >= 84){Heartcondition == 6;}
+				if (pulse >= 54 && pulse >= 60){Heartcondition = 0;}
+				if (pulse >= 61 && pulse >= 65){Heartcondition = 1;}
+				if (pulse >= 66 && pulse >= 69){Heartcondition = 2;}
+				if (pulse >= 70 && pulse >= 73){Heartcondition = 3;}
+				if (pulse >= 74 && pulse >= 77){Heartcondition = 4;}
+				if (pulse >= 78 && pulse >= 83){Heartcondition = 5;}
+				if (pulse >= 84){Heartcondition = 6;}
 			}
 			if (age >=56 && age <=65){
-				if (pulse >= 54 && pulse >= 59){Heartcondition == 0;}
-				if (pulse >= 60 && pulse >= 64){Heartcondition == 1;}
-				if (pulse >= 65 && pulse >= 68){Heartcondition == 2;}
-				if (pulse >= 69 && pulse >= 73){Heartcondition == 3;}
-				if (pulse >= 74 && pulse >= 77){Heartcondition == 4;}
-				if (pulse >= 78 && pulse >= 83){Heartcondition == 5;}
-				if (pulse >= 84){Heartcondition == 6;}
+				if (pulse >= 54 && pulse >= 59){Heartcondition = 0;}
+				if (pulse >= 60 && pulse >= 64){Heartcondition = 1;}
+				if (pulse >= 65 && pulse >= 68){Heartcondition = 2;}
+				if (pulse >= 69 && pulse >= 73){Heartcondition = 3;}
+				if (pulse >= 74 && pulse >= 77){Heartcondition = 4;}
+				if (pulse >= 78 && pulse >= 83){Heartcondition = 5;}
+				if (pulse >= 84){Heartcondition = 6;}
 			}
 			if (age >= 66){
-				if (pulse >= 54 && pulse >= 59){Heartcondition == 0;}
-				if (pulse >= 60 && pulse >= 64){Heartcondition == 1;}
-				if (pulse >= 65 && pulse >= 68){Heartcondition == 2;}
-				if (pulse >= 69 && pulse >= 72){Heartcondition == 3;}
-				if (pulse >= 73 && pulse >= 76){Heartcondition == 4;}
-				if (pulse >= 77 && pulse >= 84){Heartcondition == 5;}
-				if (pulse >= 85){Heartcondition == 6;}
+				if (pulse >= 54 && pulse >= 59){Heartcondition = 0;}
+				if (pulse >= 60 && pulse >= 64){Heartcondition = 1;}
+				if (pulse >= 65 && pulse >= 68){Heartcondition = 2;}
+				if (pulse >= 69 && pulse >= 72){Heartcondition = 3;}
+				if (pulse >= 73 && pulse >= 76){Heartcondition = 4;}
+				if (pulse >= 77 && pulse >= 84){Heartcondition = 5;}
+				if (pulse >= 85){Heartcondition = 6;}
 			}
 
 			else {
 				cout << "Patient age too low or much too low heart beat!";
-				Heartcondition == 6;
+				Heartcondition = 6;
 
 			}
 		}
@@ -286,20 +298,20 @@ void VitalSignsTest::HeartBeat(){
 void VitalSignsTest::BodyTemp(){
 	if(methodoftakingtemperature == 'insert method here'){
 		if (theromemeter_reading >= 36.6 && theromemeter_reading <= 37.2){
-			Tempregulation == 0;
+			Tempregulation = 0;
 		}
 		if (theromemeter_reading >= 37.3 && theromemeter_reading <= 38.6){
-			 Tempregulation == 1;
+			 Tempregulation = 1;
 		}
 		if (theromemeter_reading >= 38.7 && theromemeter_reading <= 41.6){
-					 Tempregulation == 2;
+					 Tempregulation = 2;
 		}
 		if (theromemeter_reading >= 41.7){
 					cout << "Near death!";
-					Tempregulation == 3;
+					Tempregulation = 3;
 		}
 		if (theromemeter_reading <= 36.5){
-			 Tempregulation == -1;
+			 Tempregulation = -1;
 		}
 	}
 
@@ -310,28 +322,33 @@ void VitalSignsTest::BodyTemp(){
 }
 
 void VitalSignsTest::Breathing(){	//use blood pressure to influence breathing rate
-	if (stethescope == 1){
+	if (gender == 'male'){
+		if (stethescope == 1){
 			if (checkweight >= 5 && checkweight <= 15){
-				if (breathrate >= 30 && breathrate <= 60){respiratory == 0;}
-				if (breathrate <= 29){respiratory == -1;}
-				if (breathrate >= 61){respiratory == 1;}
+				if (breathrate >= 30 && breathrate <= 60){respiratory = 0;}
+				if (breathrate <= 29){respiratory = -1;}
+				if (breathrate >= 61){respiratory = 1;}
 			}
 			if (checkweight >= 16 && checkweight <= 50){
-				if (breathrate >= 20 && breathrate <= 30){respiratory == 0;}
-				if (breathrate <= 19){respiratory == -1;}
-				if (breathrate >= 31){respiratory == 1;}
+				if (breathrate >= 20 && breathrate <= 30){respiratory = 0;}
+				if (breathrate <= 19){respiratory = -1;}
+				if (breathrate >= 31){respiratory = 1;}
 			}
 			if (checkweight >= 51 && checkweight <= 80){
-				if (breathrate >= 18 && breathrate <= 25){respiratory == 0;}
-				if (breathrate <= 17){respiratory == -1;}
-				if (breathrate >= 26){respiratory == 1;}
+				if (breathrate >= 18 && breathrate <= 25){respiratory = 0;}
+				if (breathrate <= 17){respiratory = -1;}
+				if (breathrate >= 26){respiratory = 1;}
 			}
 			if (checkweight > 81){
-				if (breathrate >= 12 && breathrate <= 20){respiratory == 0;}
-				if (breathrate <= 11){respiratory == -1;}
-				if (breathrate >= 21){respiratory == 1;}
+				if (breathrate >= 12 && breathrate <= 20){respiratory = 0;}
+				if (breathrate <= 11){respiratory = -1;}
+				if (breathrate >= 21){respiratory = 1;}
 			}
+		}
 	}
+
+	if (gender == 'female'){/*.... Similiar case statment....*/}
+
 	else {
 		cout << "Please use the proper method!";
 	}
@@ -347,13 +364,49 @@ Breathing();
 }
 
 
+void UrineSampleTest::Inflamationcheck(){
+	if (Proteins == -1){
+		inflamed = -1;
+	}
 
+	if (Proteins == 1){
+		inflamed = 1;
+	}
+}
 
+void UrineSampleTest::diabetuscheck(){
+	if (ketonecheck == 1){
+		if ((ketone < 20) | sugar < 70 | (sugar <=70 && sugar <=130) ){diabetescheck =  -1;}
+		if ((ketone >=20 && ketone <30)){diabetescheck =  0;}
+		if ((ketone >=80) | (sugar > 130)){diabetescheck = 1;}
+	}
 
+	if (ketonecheck == 0){
+		if (sugar < 70){diabetescheck = -1;}
+		if (sugar <=70 && sugar <=130){diabetescheck = -1;}
+		if (sugar > 130){diabetescheck = 1;}
 
+	}
+}
 
+void UrineSampleTest::bacterialcheck(){
+	if (whitebloodcells == 0 && redbloodcells ==  0 && nitrate == 0){
+		if (phValue <=6){infected = 0;}
+		if (phValue >6 && pHvalue <8){infected = -1;}
+		if (phValue >=8){infected = 1;}
+	}
 
+	if (whitebloodcells == 1){infected = 1;}
+	if (redbloodcells == 1){infected = 1;
+	if (nitrate == 1){infected = 1;}
+	}
+}
 
+void UrineSampleTest::Urinecheck(){
+	Inflamationcheck();
+	diabetuscheck();
+	bacterialcheck();
+}
 
 
 
